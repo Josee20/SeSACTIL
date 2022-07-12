d3bd57fdddc9a5430698dad4b793608596901f03
LDK <dlehdrl115@naver.com>
2022-07-12 19:48:47 +0900

    dd

---
+++ test111.playground/Contents.swift
@@ -0,0 +1,12 @@
+func basic(param: Int) -> (Int) -> Int {
+    let value = param + 20
+    
+    func append(add: Int) -> Int {
+        return value + add
+    }
+    
+    return append
+}
+
+let result = basic(param: 10)
+result(10)
